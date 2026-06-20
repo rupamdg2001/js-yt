1db83fce13148b71f0c89e38944ac71a3389614d
rupamdg2001 <167863174+rupamdg2001@users.noreply.github.com>
2026-06-20 07:24:51 +0000

    datatype advance tsummary

---
+++ 01_basic/dataypes.summary.js
@@ -0,0 +1,39 @@
+// Data Types in JavaScript
+
+// 1. Primitive Data Types
+// String , Number, Boolean, Null, Undefined, Symbol , BigInt
+
+
+const id = Symbol('123');
+const aid = Symbol('123');
+
+// console.log(id === aid); // false
+
+const bigNum = 235346456456532345n;
+// console.log(typeof bigNum); // 235346456456532345n
+
+
+
+// reference (non-primitive) data types
+// Object, Array, Function
+
+
+//arrays
+const arr = ["ruby", "python", "java"];
+
+// objects
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+// functions
+const myfun = function() {
+    console.log("Hello World");
+};
+
+console.log(typeof arr); // object
+console.log(typeof person); // object
+console.log(typeof myfun); // function
+console.log(typeof id); // symbol
