9f3fb061eb7ddfcc2bb63db37d0011a059b023c5
rupamdg2001 <167863174+rupamdg2001@users.noreply.github.com>
2026-06-20 06:01:16 +0000

    convertion of data type

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,25 @@
+// let scorev = false;
+
+// console.log(typeof scorev);
+// console.log (typeof (scorev)); 
+
+// let vnamber = Number(scorev)
+// console.log(vnamber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1
+// false => 0
+
+let nvalue = "rupam"
+let bvalue = Boolean(nvalue)
+// console.log(bvalue);
+
+// falsy values => 0, "", null, undefined, NaN, false
+// truthy values => all values except falsy values
+
+let snumberv = 33
+let snumberv2 = String(snumberv);
+console.log(snumberv2);
+console.log(typeof snumberv);
+console.log(typeof snumberv2);
