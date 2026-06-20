149e036dd2abbf8ed8612415d0b5ca1c57371adc
rupamdg2001 <167863174+rupamdg2001@users.noreply.github.com>
2026-06-20 06:57:45 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+
+// console.log("2" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);// hears a value true because null is converted to 0 and 0 >= 0 is true
+// console.log(null <= 0);// hears a alue true because null is converted to 0 and 0 <= 0 is true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// === strict equality operator
+
+// console.log(2 === 2);
+// console.log(2 === "2");
+// console.log(null === undefined);
