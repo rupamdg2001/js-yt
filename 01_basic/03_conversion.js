80debaffd17a5bb4737f0af6045e8533d70a17fe
rupamdg2001 <167863174+rupamdg2001@users.noreply.github.com>
2026-06-20 06:43:42 +0000

    convertion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -20,6 +20,29 @@ let bvalue = Boolean(nvalue)
 
 let snumberv = 33
 let snumberv2 = String(snumberv);
-console.log(snumberv2);
-console.log(typeof snumberv);
-console.log(typeof snumberv2);
+// console.log(snumberv2);
+// console.log(typeof snumberv);
+// console.log(typeof snumberv2);
+
+// -------------------------------operation-----------------------------//
+
+let value = 3 ;
+let negvalue = -value
+// console.log(negvalue , value);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+// console.log(1 + "2" + 3);
+
+// console.log(true + true);
+// console.log(true+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+//follow mdn for prefix and postfix increment and decrement operator//
